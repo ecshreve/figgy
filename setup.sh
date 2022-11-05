c214ff614f0d65e2dbf296d146796e967f7f5479
shreve <eric@shreve.dev>
2022-11-05 19:18:19 +0000

    basic setup script and example fish locals

---
+++ setup.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd "$HOME" || exit
+
+if ! test -d "$HOME/.cfg" ; then
+  echo Cloning dotfiles
+
+  git clone --bare https://github.com/ecshreve/figgy.git "$HOME/.cfg"
+  git --git-dir="$HOME/.cfg/" --work-tree="$HOME" checkout
+  git --git-dir="$HOME/.cfg/" --work-tree="$HOME" pull --force
+fi
+
+if [[ "$OSTYPE" = "darwin"* ]] ; then
+  if ! xcode-select -p 1>/dev/null 2>&1 ; then
+    echo Installing XCode tools
+
+    xcode-select --install
+    read -rp "Press enter to continue"
+  fi
+
+  if ! test -f /opt/local/bin/port ; then
+    echo Installing MacPorts
+
+    echo Go here and find the pkg url:
+    echo https://www.macports.org/install.php
+    open "https://www.macports.org/install.php"
+    read -p "url? " url
+    curl --location $url --output macports.pkg
+    open macports.pkg
+    read -rp "Press enter to continue"
+    rm macports.pkg
+  fi
+fi
+
+if ! which fish 1>/dev/null 2>&1 ; then
+  echo Installing Fish
+
+  if [[ $OSTYPE = darwin* ]] ; then
+    sudo /opt/local/bin/port install fish
+  elif which apt-get 1>/dev/null 2>&1 ; then
+    sudo apt-add-repository ppa:fish-shell/release-3 -y
+    sudo apt-get install fish -y
+  fi
+fi
+
+fish -c 'setup'
