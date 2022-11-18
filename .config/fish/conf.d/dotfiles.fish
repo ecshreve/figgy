6a2ddec58d8d5d81f3424d11f2d85be1fc5fc7f9
shreve <eric@shreve.dev>
2022-11-18 04:48:50 +0000

    add some fisher plugins

--- .config/fish/conf.d/dotfiles.fish
+++ .config/fish/conf.d/dotfiles.fish
@@ -3,16 +3,5 @@ debug-fish-init start (status -f)
 if test -d "$HOME/.cfg"
     alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
     config config --local status.showUntrackedFiles no
-
-    abbr -a caa config add
-    abbr -a cmsg config commit -m
-    abbr -a cush config push
-    abbr -a cst config status
-    abbr -a ciff config diff
-    abbr -a clog config log
-
-    abbr -a gst git status
-    abbr -a gcmsg git commit -m 
-
 end
 debug-fish-init end (status -f)
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,21 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # commands to run in interactive sessions can go here
     set fzf_fd_opts --hidden --no-ignore --exclude=.git
     fzf_configure_bindings --directory=\cff --git_log=\cfl --git_status=\cfs --processes=\cfp --variables=\cfv
+
+    # preserve scrollback buffer
+    alias clear="clear -x"
+
+    # convenience abbreviations for config git repo
+    abbr -a caa config add
+    abbr -a cmsg config commit -m
+    abbr -a cush config push
+    abbr -a cst config status
+    abbr -a ciff config diff
+    abbr -a clog config log
+
+    # git aliases
+    abbr -a gst git status
+    abbr -a gcmsg git commit -m 
 
 end
