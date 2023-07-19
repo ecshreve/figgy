fce2a043eee01a749ba4a9c7967ce5bf8a2e0d50
Eric <eric@shreve.dev>
2023-07-19 00:24:43 -0700

    Update dotfiles.fish

--- .config/fish/conf.d/dotfiles.fish
+++ .config/fish/conf.d/dotfiles.fish
@@ -1,7 +1,8 @@
 # set up config repo
 debug-fish-init start (status -f)
 if test -d "$HOME/.cfg"
-    alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-    config config --local status.showUntrackedFiles no
+    echo "AHH SOMETHING IS WRONG"
+    # alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+    # config config --local status.showUntrackedFiles no
 end
 debug-fish-init end (status -f)
