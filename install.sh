8f71d2ef4d0213b6121d88aa69b6bec3862fd026
shreve <eric@shreve.dev>
2023-09-19 03:54:43 -0700

    fix: change config path

--- install.sh
+++ install.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
 
-cd $HOME
+set -x 
 
-if test -d "$HOME/.config/fish" ; then
-	rm -rf ~/.config/fish
+cd "$HOME" || exit
+
+# check for git installation
+if ! [ -x "$(command -v git)" ]; then
+  echo 'git is not installed' >&2
+  exit 1
 fi
 
-cp -r ~/dotfiles/.config/fish ~/.config
+# check for existing config repo
+if ! test -d "$HOME/.cfg" ; then
+  echo Cloning dotfiles
+  git clone https://github.com/ecshreve/figgy.git "$HOME/.cfg"
+
+  # git clone --bare https://github.com/ecshreve/figgy.git "$HOME/.cfg"
+  # git --git-dir="$HOME/.cfg/" --work-tree="$HOME" checkout
+  # git --git-dir="$HOME/.cfg/" --work-tree="$HOME" pull --force
+fi
+
+rm -rf ~/.config/fish
+mkdir -p ~/.config/fish
+cp -r ~/.cfg/.config/fish ~/.config/
+cp ~/.cfg/.gitconfig ~/.gitconfig
 
 fish -c 'setup'
 
